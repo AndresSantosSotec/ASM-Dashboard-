6638463cc80222eefabff5cb259d389881116248
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-06-03 22:07:08 -0600

    asdf

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -389,14 +389,14 @@ export default function Sidebar({ open, className }: SidebarProps) {
                   <UserCheck size={16} className="mr-2" />
                   <span>Estatus Académico</span>
                 </Link>
-                <Link
+                {/* <Link
                   href="/academico/estado-sistema"
                   className={`flex items-center px-4 py-1.5 rounded-md ${pathname === "/academico/estado-sistema" ? "bg-asm-medium-gold text-white" : "text-asm-light-gold hover:bg-asm-medium-gold/20"
                     } transition-colors duration-200`}
                 >
                   <Activity size={16} className="mr-2" />
                   <span>Estatus General</span>
-                </Link>
+                </Link> */}
                 <Link
                   href="/academico/ranking"
                   className={`flex items-center px-4 py-1.5 rounded-md ${pathname === "/academico/ranking" ? "bg-asm-medium-gold text-white" : "text-asm-light-gold hover:bg-asm-medium-gold/20"
