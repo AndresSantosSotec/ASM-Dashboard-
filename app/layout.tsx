1b7823a27ad06b473b512ee7d413f9ce634e7890
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-11-03 19:24:59 -0600

    FIX: Update favicon and add new icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,14 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
+      { url: "/icons/ASM.ico" },
       { url: "/icon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
+    shortcut: "/icons/ASM.ico",
   },
   manifest: "/site.webmanifest",
 }
